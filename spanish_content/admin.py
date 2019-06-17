536ccf81eeeded59a37d3808d6beb89a5ac3bfc8
Rachell Calhoun <rachell.calhoun@gmail.com>
2019-06-17 13:49:25 -0400

    update admin bug

--- spanish_content/admin.py
+++ spanish_content/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import OurStory, OurTeam, EduProgram, EthicalPost, VolunteerPeru, VolunteerOpenPosition, VolunteerAbout, CustomPage, Home, FooterInfo, DonateSection, Menu, SubMenu, HomeLink, Apply
+from .models import (
+    OurStory, OurTeam, EduProgram, EthicalPost, VolunteerPeru, VolunteerOpenPosition,
+    VolunteerAbout, CustomPage, Home, FooterInfo, DonateSection, Menu, SubMenu, HomeLink,
+    Apply
+)
 from django.contrib.flatpages.admin import FlatpageForm, FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage
 from tinymce.widgets import TinyMCE
@@ -11,7 +15,7 @@ class OurStoryAdmin(admin.ModelAdmin):
     list_display=('order','img','text', 'color')
 
 class HomeAdmin(admin.ModelAdmin):
-    list_display = ('order','title','color','img','text')
+    list_display = ('id', 'title','order','color','img','text')
     list_editable = ('order',)
 
 class OurTeamAdmin(admin.ModelAdmin):
@@ -40,9 +44,6 @@ class MCEFlatPageForm(FlatpageForm):
 
 
 class MCEFlatPageAdmin(FlatPageAdmin):
-    """
-    Page Admin
-    """
     form = MCEFlatPageForm
 
 
@@ -59,7 +60,7 @@ class VolunteerAboutAdmin(admin.ModelAdmin):
     list_display = ('order', 'title' )
 
 class CustomPageAdmin(admin.ModelAdmin):
-    list_display = ('category', 'order','title')
+    list_display = ('id', 'category', 'order','title')
     list_editable = ('order',)
     list_filter = ('category',)
 
