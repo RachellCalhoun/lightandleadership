449660064aff37ae9aebfa6556404798ad724e23
Rachell Calhoun <rachell.calhoun@gmail.com>
2019-08-10 10:26:38 -0400

    flatpage navbar langauge update

--- content/context_processors.py
+++ content/context_processors.py
@@ -5,8 +5,12 @@ def footer(request):
 	return {'footer': footer}
 
 def navbar(request):
+	if 'es/' in request.get_full_path():
+		english = False
+	else:
+		english = True
 	categories = Menu.objects.all().order_by('order')
-	return {'categories': categories}
+	return {'categories': categories, 'english': english}
 
 def global_analytic(request):
 	analytic_tags = AnalyticTag.objects.all()
--- spanish_content/context_processors.py
+++ spanish_content/context_processors.py
@@ -4,7 +4,11 @@ def footer(request):
     footer = FooterInfo.objects.all()
     return {'footer': footer}
 
-def spanish_navbar(request):
-    categories = Menu.objects.all().order_by('order')
-    return {'spanish_categories': categories}
 
+def spanish_navbar(request):
+    if 'es/' in request.get_full_path():
+        english = False
+    else:
+        english = True
+    categories = Menu.objects.all().order_by('order')
+    return {'spanish_categories': categories, 'english': english}
