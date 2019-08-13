c76bc3cc347c918cd1c20d0fecef44f93361bef8
Rachell Calhoun <rachell.calhoun@gmail.com>
2019-08-12 21:40:30 -0400

    template clean up, update spanish urls

--- content/context_processors.py
+++ content/context_processors.py
@@ -1,16 +1,13 @@
 from .models import FooterInfo, Menu, AnalyticTag
 
-def footer(request):
-	footer = FooterInfo.objects.all()
-	return {'footer': footer}
-
 def navbar(request):
 	if 'es/' in request.get_full_path():
 		english = False
 	else:
 		english = True
 	categories = Menu.objects.all().order_by('order')
-	return {'categories': categories, 'english': english}
+	footer = FooterInfo.objects.all()
+	return {'categories': categories, 'english': english, 'footer': footer}
 
 def global_analytic(request):
 	analytic_tags = AnalyticTag.objects.all()
--- llisite/settings.py
+++ llisite/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'content.context_processors.footer',
+                # 'content.context_processors.footer',
                 'content.context_processors.navbar',
                 'content.context_processors.global_analytic',
                 'spanish_content.context_processors.spanish_navbar',
